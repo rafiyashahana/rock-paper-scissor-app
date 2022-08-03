f4cd6c8aba023e60b3306838efb785d52c6b7a28
shahana <rafiyashahana@gmail.com>
2022-08-03 19:46:49 +0300

    added logic

--- script.js
+++ script.js
@@ -1,78 +1,74 @@
-/*
-  Rock Paper Scissors 🚀🔥
-  Concepts covered in this project
-    👉 For loops
-    👉 Dom Manipulation
-    👉 Variables
-    👉 Conditionals (if else if)
-    👉 Template Literals
-    👉 Event Listeners
-    👉 Higher order Function (Math.random())
-*/
+const computerChoiceDiv = document.getElementById("computerChoice");
 
-// ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
-// getComputerChoice() 👉 'Rock'
-// getComputerChoice() 👉 'Scissors'
 function getComputerChoice() {
-  
+  const options = ["✊", "🤚", "✌"];
+  const random = Math.floor(Math.random() * options.length);
+  return options[random];
 }
 
-// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
-// human wins - getResult('Rock', 'Scissors') 👉 1
-// human loses - getResult('Scissors', 'Rock') 👉 -1
-// human draws - getResult('Rock', 'Rock') 👉 0
 function getResult(playerChoice, computerChoice) {
-  // return the result of score based on if you won, drew, or lost
-  
-  
-
-  // All situations where human draws, set `score` to 0
-  
-
-  // All situations where human wins, set `score` to 1
-  // make sure to use else ifs here
-  
-
-  // Otherwise human loses (aka set score to -1)
-  
-
-  // return score
-  
+  let score;
+  if (playerChoice === computerChoice) {
+    score = 0;
+  } else if (
+    (playerChoice == "✊" && computerChoice == "🤚") ||
+    (playerChoice == "🤚" && computerChoice == "✌") ||
+    (playerChoice == "✌" && computerChoice == "✊")
+  ) {
+    score = -1;
+  } else {
+    score = 1;
+  }
+  return score;
 }
 
 // ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
+const resultDiv = document.getElementById("result");
+resultDiv.innerText = `\n`;
 function showResult(score, playerChoice, computerChoice) {
-  // Hint: on a score of -1
-  // You should do result.innerText = 'You Lose!'
-  // Don't forget to grab the div with the 'result' id!
+  if (score == -1) {
+    resultDiv.innerText = "You Lose!";
+  } else if (score == 1) {
+    resultDiv.innerText = "You Win!";
+  } else {
+    resultDiv.innerText = "It's a Draw!";
+  }
 }
 
 // ** Calculate who won and show it on the screen **
-function onClickRPS(playerChoice) {
-  
-}
+const rpsbuttonDiv = document.getElementsByClassName("rpsButton");
 
+const yourChoiceDiv = document.getElementById("yourChoice");
+function onClickRPS(playerChoice) {
+  yourChoiceDiv.innerHTML = `You Selected: ${playerChoice}`;
+  return playerChoice;
+}
 
 // ** Make the RPS buttons actively listen for a click and do something once a click is detected **
 function playGame() {
-  // use querySelector to select all RPS Buttons
+  const rpsBtns = document.querySelectorAll(".rpsButton");
 
-  // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
-  
-  // 1. loop through the buttons using a forEach loop
-  // 2. Add a 'click' event listener to each button
-  // 3. Call the onClickRPS function every time someone clicks
-  // 4. Make sure to pass the currently selected rps button as an argument
+  rpsBtns.forEach(
+    (btn) =>
+      (btn.onclick = () => {
+        const res = getComputerChoice();
+        computerChoiceDiv.innerText = res;
+        onClickRPS(btn.value);
 
- 
-
-  // Add a click listener to the end game button that runs the endGame() function on click
-  
+        showResult(getResult(btn.value, res), btn.value, res);
+      })
+  );
 }
 
 // ** endGame function clears all the text on the DOM **
 function endGame() {
-  
+  computerChoiceDiv.innerText = "";
+  yourChoiceDiv.innerHTML = "";
+  resultDiv.innerText = `\n`;
 }
+const endGameButtonDiv = document.getElementById("endGameButton");
+endGameButtonDiv.onclick = () => endGame();
 
-playGame()
+playGame();
+
+// const emojis = {rock: "✊", paper: "🤚", scissors: "✌"};
